6a2da13b4c912f0b072ad920cc76182f6fb52c86
illya.dergun@gmail.com <bkmz25061997>
2018-11-03 21:39:50 +0200

    commit: dao lay has been created
    - entities package and interfaces package has been created

---
+++ src/main/java/cinemaproject/illiaderhun/com/github/dao/entities/User.java
@@ -0,0 +1,142 @@
+package cinemaproject.illiaderhun.com.github.dao.entities;
+
+import java.util.Objects;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private Integer roleId;
+
+    public User(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.surname = builder.surname;
+        this.email = builder.email;
+        this.password = builder.password;
+        this.roleId = builder.roleId;
+    }
+
+    public static class Builder {
+        // required fields
+        private String name;
+        private String email;
+
+        // optional fields
+        private Integer id = 0;
+        private String surname = "unknown";
+        private String password = "unknown";
+        private Integer roleId = 0;
+
+        public Builder(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        public Builder userId(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder surname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder roleId(Integer roleId) {
+            this.roleId = roleId;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(surname, user.surname) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(roleId, user.roleId);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name, surname, email, password, roleId);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", roleId=" + roleId +
+                '}';
+    }
+}
---
+++ src/main/java/cinemaproject/illiaderhun/com/github/dao/interfaces/MovieDao.java
@@ -0,0 +1,8 @@
+package cinemaproject.illiaderhun.com.github.dao.interfaces;
+
+public interface MovieDao<Movie, Integer> extends Dao<Movie, Integer> {
+
+    Movie readByTitle(String title);
+    Movie readByScheduleId(Integer scheduleId);
+
+}
